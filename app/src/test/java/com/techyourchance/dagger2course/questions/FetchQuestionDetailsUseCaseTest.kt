a81a6f2d3bee3e982c5d48eaa9f9371938ee90cb
Carlo Trajano <carlo.trajano@gmail.com>
2025-02-12 00:12:23 +0800

    Completed FetchQuestionDetailsUseCaseTest and FetchQuestionsUseCaseTest

--- app/src/test/java/com/techyourchance/dagger2course/questions/FetchQuestionDetailsUseCaseTest.kt
+++ app/src/test/java/com/techyourchance/dagger2course/questions/FetchQuestionDetailsUseCaseTest.kt
@@ -2,24 +2,25 @@ package com.techyourchance.dagger2course.questions
 
 import com.techyourchance.dagger2course.networking.SingleQuestionResponseSchema
 import com.techyourchance.dagger2course.networking.StackoverflowApi
+import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase.Result
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import retrofit2.Response
-import retrofit2.Retrofit
+import kotlin.coroutines.cancellation.CancellationException
 
 class FetchQuestionDetailsUseCaseTest {
 
     private lateinit var fetchQuestionDetailsUseCase: FetchQuestionDetailsUseCase
 
     @MockK
-    private lateinit var stackOverflowApi: StackoverflowApi
-
-    @MockK
-    private lateinit var retrofit: Retrofit
+    private val stackoverflowApi: StackoverflowApi = mockk()
 
     private val testData =
         QuestionWithBody(
@@ -28,26 +29,76 @@ class FetchQuestionDetailsUseCaseTest {
             body = "body",
         )
 
+    private val questionId = "id"
+
+
 
     @BeforeEach
     fun setUp() {
-        coEvery { stackOverflowApi.questionDetails(questionId = any()) } returns Response.success(
-            SingleQuestionResponseSchema(questions = listOf(testData))
-        )
-
-        fetchQuestionDetailsUseCase = FetchQuestionDetailsUseCase(retrofit = retrofit)
+        fetchQuestionDetailsUseCase = FetchQuestionDetailsUseCase(stackoverflowApi = stackoverflowApi)
     }
 
     @Test
     fun `Should fetch question details successfully`() {
-        val expected = FetchQuestionDetailsUseCase.Result.Success(questionBody = testData.body)
+        val expected = Result.Success(questionBody = testData.body)
         val questionId = "id"
 
-        coEvery { fetchQuestionDetailsUseCase.fetchQuestionDetails(questionId = questionId) } returns expected
+        coEvery { stackoverflowApi.questionDetails(questionId = any()) } returns Response.success(
+            SingleQuestionResponseSchema(questions = listOf(testData))
+        )
 
         runTest {
             val actual = fetchQuestionDetailsUseCase.fetchQuestionDetails(questionId = questionId)
             assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `Should prompt failure when fetch question details returns null response body`() {
+        val expected = Result.Failure
+
+        coEvery { stackoverflowApi.questionDetails(questionId = any()) } returns Response.success(null)
+
+        runTest {
+            val actual = fetchQuestionDetailsUseCase.fetchQuestionDetails(questionId = questionId)
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `Should prompt failure when fetch question details fails`() {
+        val expected = Result.Failure
+
+        coEvery { stackoverflowApi.questionDetails(any()) } returns Response.error(
+            404,
+            ResponseBody.create(null, "")
+        )
+
+        runTest {
+            val actual = fetchQuestionDetailsUseCase.fetchQuestionDetails(questionId = questionId)
+            assertEquals(expected, actual)
+
+        }
+    }
+
+    @Test
+    fun `Should prompt failure when fetch question details throws an exception`() {
+        val expected = Result.Failure
+
+        coEvery { stackoverflowApi.questionDetails(any()) } throws RuntimeException()
+
+        runTest {
+            val actual = fetchQuestionDetailsUseCase.fetchQuestionDetails(questionId = questionId)
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `Should throw exception when fetch question details throws CancellationException`() {
+        coEvery { stackoverflowApi.questionDetails(any()) } throws CancellationException()
+
+        runTest {
+            assertThrows<CancellationException>(message = { "Throwing Cancellation Exception"}) { fetchQuestionDetailsUseCase.fetchQuestionDetails(questionId = questionId) }
         }
     }
 }
--- app/src/test/java/com/techyourchance/dagger2course/questions/FetchQuestionsUseCaseTest.kt
+++ app/src/test/java/com/techyourchance/dagger2course/questions/FetchQuestionsUseCaseTest.kt
@@ -1,21 +1,24 @@
 package com.techyourchance.dagger2course.questions
 
+import com.techyourchance.dagger2course.networking.QuestionsListResponseSchema
+import com.techyourchance.dagger2course.networking.StackoverflowApi
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import retrofit2.Retrofit
+import org.junit.jupiter.api.assertThrows
+import retrofit2.Response
+import kotlin.coroutines.cancellation.CancellationException
 
 class FetchQuestionsUseCaseTest {
-    // TODO Incompatible for testing because we can't mock everything
-
     private lateinit var fetchQuestionsUseCase: FetchQuestionsUseCase
 
     @MockK
-    private lateinit var retrofit: Retrofit
+    private val stackoverflowApi: StackoverflowApi = mockk()
 
     private val testData = listOf(
         Question(title = "Title 1", "id1"),
@@ -26,7 +29,7 @@ class FetchQuestionsUseCaseTest {
 
     @BeforeEach
     fun setUp() {
-        fetchQuestionsUseCase = FetchQuestionsUseCase(retrofit = retrofit)
+        fetchQuestionsUseCase = FetchQuestionsUseCase(stackoverflowApi = stackoverflowApi)
     }
 
     @Test
@@ -35,25 +38,64 @@ class FetchQuestionsUseCaseTest {
             data = testData
         )
 
-        coEvery { fetchQuestionsUseCase.fetchLatestQuestions() } returns expected
+        coEvery { stackoverflowApi.lastActiveQuestions(any()) } returns Response.success(
+            QuestionsListResponseSchema(
+                questions = testData
+            )
+        )
 
         runTest {
             val actual = fetchQuestionsUseCase.fetchLatestQuestions()
             assertEquals(expected, actual)
         }
+    }
 
+    @Test
+    fun `Should prompt failure when fetch question returns success with empty response body`() {
+        val expected = FetchQuestionsUseCase.Result.Failure
+
+        coEvery { stackoverflowApi.lastActiveQuestions(any()) } returns Response.success(null)
+
+        runTest {
+            val actual = fetchQuestionsUseCase.fetchLatestQuestions()
+            assertEquals(expected, actual)
+        }
     }
 
     @Test
     fun `Should prompt failure when fetch question fails`() {
         val expected = FetchQuestionsUseCase.Result.Failure
 
-        coEvery { fetchQuestionsUseCase.fetchLatestQuestions() } returns expected
+        coEvery { stackoverflowApi.lastActiveQuestions(any()) } returns Response.error(
+            404,
+            ResponseBody.create(null, "")
+        )
 
         runTest {
             val actual = fetchQuestionsUseCase.fetchLatestQuestions()
             assertEquals(expected, actual)
 
+        }
+    }
+
+    @Test
+    fun `Should prompt failure when fetch question throws an exception`() {
+        val expected = FetchQuestionsUseCase.Result.Failure
+
+        coEvery { stackoverflowApi.lastActiveQuestions(any()) } throws RuntimeException()
+
+        runTest {
+            val actual = fetchQuestionsUseCase.fetchLatestQuestions()
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `Should throw exception when fetch question throws CancellationException`() {
+        coEvery { stackoverflowApi.lastActiveQuestions(any()) } throws CancellationException()
+
+        runTest {
+            assertThrows<CancellationException>(message = { "Throwing Cancellation Exception"}) { fetchQuestionsUseCase.fetchLatestQuestions() }
         }
     }
 }
