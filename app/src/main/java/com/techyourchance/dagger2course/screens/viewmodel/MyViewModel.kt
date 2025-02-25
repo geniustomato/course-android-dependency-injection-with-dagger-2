128e45815285bfb0ea5b3603c00f518715d8c321
Carlo Trajano <carlo.trajano@gmail.com>
2025-02-25 16:36:07 +0800

    Add initial ViewModel setup for ViewModelActivity

---
+++ app/src/main/java/com/techyourchance/dagger2course/screens/viewmodel/MyViewModel.kt
@@ -0,0 +1,27 @@
+package com.techyourchance.dagger2course.screens.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
+import com.techyourchance.dagger2course.questions.Question
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MyViewModel @Inject constructor(
+    private val fetchQuestionsUseCase: FetchQuestionsUseCase,
+) : ViewModel() {
+
+    private val _questions = MutableLiveData<List<Question>>()
+    val question: LiveData<List<Question>> = _questions
+
+    init {
+        viewModelScope.launch {
+            when (val result = fetchQuestionsUseCase.fetchLatestQuestions()) {
+                is FetchQuestionsUseCase.Result.Success -> _questions.value = result.data
+                FetchQuestionsUseCase.Result.Failure -> throw RuntimeException("Fetch failed")
+            }
+        }
+    }
+}
